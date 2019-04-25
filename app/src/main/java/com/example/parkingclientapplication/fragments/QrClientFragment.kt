9315401cef86af0f66c9a1ec2a6718101449a2cb
Alejandro <alegorval@gmail.com>
2019-04-25 18:54:35 +0200

    Fixed QrFragment

--- app/src/main/java/com/example/parkingclientapplication/fragments/QrClientFragment.kt
+++ app/src/main/java/com/example/parkingclientapplication/fragments/QrClientFragment.kt
@@ -1,102 +1,122 @@
 package com.example.parkingclientapplication.fragments
 
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
 import android.widget.Toast
-import at.nineyards.anyline.camera.CameraOpenListener
 
 import com.example.parkingclientapplication.R
-import java.lang.Exception
-import android.widget.TextView
-import at.nineyards.anyline.camera.CameraController
-import io.anyline.plugin.barcode.BarcodeScanViewPlugin
-import io.anyline.view.ScanView
-import io.anyline.view.BaseScanViewConfig
-import io.anyline.view.ScanViewPluginConfig
-import android.view.WindowManager
+import android.widget.ImageView
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.BarcodeFormat
+import android.graphics.Color
+import android.support.v4.app.Fragment
+import android.view.*
+import android.widget.Button
+import com.google.zxing.EncodeHintType
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.common.BitMatrix
+import java.util.*
 
 
+class QrClientFragment : Fragment() {
 
+    // barcode data
+    val BARCODE_DATA = "123456"
+    //QR Code data
+    val QR_CODE_DATA = "www.skholingua.com"
 
-class QrClientFragment : Fragment(), CameraOpenListener {
+    // barcode image
+    var bitmap: Bitmap? = null
+    var outputImage: ImageView? = null
 
+    private lateinit var qr_code_btn: Button
+    private lateinit var barcode_btn: Button
+    private lateinit var scanner_btn: Button
 
-    private var barcodeScanView: ScanView? = null
-    private var resultText: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_qr_client, container, false)
-        //Set the flag to keep the screen on (otherwise the screen may go dark during scanning)
-        activity!!.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        outputImage =view!!.findViewById(R.id.imageView)
+        qr_code_btn = view.findViewById(R.id.qr_code_btn)
+        barcode_btn = view.findViewById(R.id.barcode_btn)
+        scanner_btn = view.findViewById(R.id.scanner_btn)
 
-        resultText = view.findViewById(R.id.text_result) as TextView
+        qr_code_btn.setOnClickListener {
+            bitmap = encodeAsBitmap(QR_CODE_DATA, BarcodeFormat.QR_CODE, 512, 512)
+            outputImage!!.setImageBitmap(bitmap)
+        }
 
-        barcodeScanView = view.findViewById(R.id.scan_view) as ScanView
-        // add a camera open listener that will be called when the camera is opened or an error occurred
-        //  this is optional (if not set a RuntimeException will be thrown if an error occurs)
-        barcodeScanView!!.setCameraOpenListener(this)
-        // the view can be configured via a json file in the assets, and this config is set here
-        // (alternatively it can be configured via xml, see the Energy Example for that)
-        //barcodeScanView.setScanConfig("barcode_view_config.json");
+        barcode_btn.setOnClickListener {
+            bitmap = encodeAsBitmap(BARCODE_DATA, BarcodeFormat.CODE_128, 600, 300)
+            outputImage!!.setImageBitmap(bitmap)
+        }
 
-        // optionally limit the barcode format to (multiple) specific types
-        //barcodeScanView.setBarcodeFormats(BarcodeScanView.BarcodeFormat.QR_CODE,
-        //        BarcodeScanView.BarcodeFormat.CODE_128);
-        //init the scanViewPlugin configuration which hold the scan view ui configuration (cutoutConfig and ScanFeedbackConfig)
-        val barcodeScanviewPluginConfig = ScanViewPluginConfig(activity!!.applicationContext, "barcode_view_config.json")
-        //init the scan view
-        val scanViewPlugin = BarcodeScanViewPlugin(
-            activity!!.applicationContext,
-            getString(R.string.anyline_license_key),
-            barcodeScanviewPluginConfig,
-            "BARCODE"
-        )
-        //init the base scanViewconfig which hold camera and flash configuration
-        val barcodeBaseScanViewConfig = BaseScanViewConfig(activity!!.applicationContext, "barcode_view_config.json")
-        //set the base scanViewConfig to the ScanView
-        barcodeScanView!!.setScanViewConfig(barcodeBaseScanViewConfig)
-        //set the scanViewPlugin to the ScanView
-        barcodeScanView!!.scanViewPlugin = scanViewPlugin
-        //add result listener
-        scanViewPlugin.addScanResultListener { result -> resultText!!.text = result.result.toString() }
+        scanner_btn.setOnClickListener {
+            val integrator = IntentIntegrator(this.activity)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)   //To scan all types of Barcodes
+            integrator.setPrompt("Scan")   //Set message as SCAN
+            integrator.setCameraId(0)  //Default camera as back camera/main camera
+            integrator.setBeepEnabled(false)   //Enable scan sound for success or failure
+            integrator.initiateScan()
+        }
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return view
     }
 
-    override fun onResume() {
-        super.onResume()
-        resultText!!.text = ""
-        //start the actual scanning
-        barcodeScanView!!.start()
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+
+        inflater!!.inflate(R.menu.client_map, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
-    override fun onPause() {
-        super.onPause()
-        //stop the scanning
-        barcodeScanView!!.stop()
-        //release the camera (must be called in onPause, because there are situations where
-        // it cannot be auto-detected that the camera should be released)
-        barcodeScanView!!.releaseCameraInBackground()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
-
-    override fun onCameraOpened(cameraController: CameraController, width: Int, height: Int) {
-        //the camera is opened async and this is called when the opening is finished
-
+    private fun encodeAsBitmap(contents:String?, format:BarcodeFormat, imgWidth:Int, imgHeight:Int): Bitmap? {
+        if (contents == null)
+        {
+            return null
+        }
+        val hints = EnumMap<EncodeHintType, Any>(EncodeHintType::class.java)
+        hints[EncodeHintType.CHARACTER_SET] = "UTF-8"
+        val writer = MultiFormatWriter()
+        val result: BitMatrix
+        try
+        {
+            result = writer.encode(contents, format, imgWidth, imgHeight, hints)
+        }
+        catch (iae:IllegalArgumentException) {
+            // Unsupported format
+            return null
+        }
+        val width = result.width
+        val height = result.height
+        val pixels = IntArray(width * height)
+        for (y in 0 until height)
+        {
+            val offset = y * width
+            for (x in 0 until width)
+            {
+                pixels[offset + x] = if (result.get(x, y)) Color.BLACK else Color.WHITE
+            }
+        }
+        val bitmap = Bitmap.createBitmap(width, height,
+            Bitmap.Config.ARGB_8888)
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height)
+        return bitmap
     }
-
-    override fun onCameraError(e: Exception) {
-        //This is called if the camera could not be opened.
-        // (e.g. If there is no camera or the permission is denied)
-        // This is useful to present an alternative way to enter the required data if no camera exists.
-        throw RuntimeException(e)
-    }
-
-
 }
+
+
